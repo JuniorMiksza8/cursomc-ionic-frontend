e8ae50d31306f9f0fa3d7affeb1010a814be47fb
Junior Miksza <junior.miksza8@gmail.com>
2020-03-09 17:25:22 -0300

    Armazenamento dos dados do pedido

--- src/pages/pickaddress/pickaddress.ts
+++ src/pages/pickaddress/pickaddress.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -12,15 +14,26 @@ import { StorageService } from '../../services/storage.service';
 export class PickaddressPage {
 
   items : EnderecoDTO[];
+  pedido : PedidoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public clienteService : ClienteService,public storage : StorageService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public clienteService : ClienteService,public storage : StorageService,public cartService:CartService) {
   }
 
   ionViewDidLoad() {
+
     let localUser = this.storage.getLocalUser();
       if(localUser && localUser.email){
         this.clienteService.findByEmail(localUser.email).subscribe(response =>{
           this.items = response['enderecos'];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente:{id : response['id']},
+            enderecoDeEntrega : null,
+            pagamento : null,
+            itens : cart.itens.map(x =>{return {quantidade: x.quantidade ,produto: {id : x.produto.id} }})
+          };
         },
         error =>{
             if(error.status == 403){
@@ -30,6 +43,12 @@ export class PickaddressPage {
       }else{
         this.navCtrl.setRoot("HomePage");
       }
+      
+  }
+
+  nextPage(item : EnderecoDTO){
+    this.pedido.enderecoDeEntrega = {id : item.id};
+    console.log(this.pedido);
   }
 
 }
